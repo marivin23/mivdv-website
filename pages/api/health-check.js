0782347ab67d146a08133b2dd16b2247e2a42c9f
mariusirimie <marius.irimie@rodeapps.com>
2021-09-07 23:05:03 +0300

    Fixes.

--- pages/api/health-check.js
+++ pages/api/health-check.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Mivdv API works' })
+    if (req.method === 'GET') {
+        res.status(200);
+        res.send('success');
+    }
 }
